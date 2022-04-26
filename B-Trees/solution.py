b9ddc0d1ec89a000c4f6701184238905f2d379bd
Dominik <djmmatracki@gmail.com>
2022-04-26 22:46:18 +0200

    solution

--- B-Trees/solution.py
+++ B-Trees/solution.py
@@ -2,14 +2,19 @@
 
 def insertKey(node, key):
     """Insert to leaf"""
+    
+    # Sprawdzam czy wezel nie jest pelny
     if None not in node.keys:
+        # Podzielic wezel, poniewaz node jest pelny
+        # Zwraca srodkowy klucz z podzialu i wskazanie na nowo utworzony wezel
         return False
 
+    # Wpisac w odpowiednie miejsce nowy klucz
     i = 0
     while i < len(node.keys):
         if node.keys[i] is None:
             node.keys[i] = key
-            return True
+            return
 
         if node.keys[i] > key:
             i -= 1
@@ -24,7 +29,6 @@ def insertKey(node, key):
         temp = temp1
         i += 1
 
-    return True
 
 
 def isLeaf(node):
@@ -40,6 +44,18 @@ class BNode:
         self.children = [None for _ in range(max_keys+1)]
 
 
+# Przekuje aktualny wezel w przeszukaniu klucza wiekrzego od znalezionego
+# Sprawdza czy jest lisciem
+# Jezeli jest to dodaje klucz
+# Jezeli nie to wola sie rekurencyjnie dla lewego potomka
+# Po powrocie moze sie okazac ze potomek do ktorego przeszlismy zostal podzielony
+# Tak wiec nalezy dodac do aktualnego wezla srodkowy klucz podzialy i wskazanie na nowo utworzony wezel
+# Funkcja otrzymuje dodawany klucz oraz wskazanie na nowo-utworzony wezel ze swego poprzedniego wywolania
+# Musi ona sprawdzic czy wezel nie jest pelny
+#
+
+
+
 class BTree:
     def __init__(self, max_keys) -> None:
         self.root = BNode(max_keys)
@@ -49,6 +65,7 @@ class BTree:
         def insertToNode(node, key):
             i = 0
 
+            # Szukamy miejsca w ktore trzeba wstawic node
             while i < self.max_keys:
                 if node.keys[i] is None:
                     break
@@ -58,17 +75,23 @@ class BTree:
                 i += 1
 
             if isLeaf(node):
-                # Dodajemy do wezla klucz
-                # Jezeli wezel jest pelny to zwracamy False
-                # Jezeli poprawnie wstawilismy to True
-                added = insertKey(node, key)
-                if added:
-                    # Zwracamy ze nie nastapilo podzialu
-                    return False
+                result = insertKey(node, key)
+                # Niema miejsca w leafie
+                if result is None:
+                    return
+                # Zwracamy srodkowy klucz ktory musi byc dodany do parenta
+                # oraz liste kluczy ktore musza byc przypisane do nowego noda
+                return result
 
-                # Musi natapic podzial
-                return True
+            result = insertToNode(node.children[i], key)
+
+            if result is None:
+                # Wezel nie zostal podzielony, konczymy
+                return
+
+            middleKey, newKeys = result
             
-            return insertToNode(node.children[i], key)
-
-        return insertToNode(self.root, key)
+            if None not in node.keys:
+                # Parent jest pelny
+                pass
+    
